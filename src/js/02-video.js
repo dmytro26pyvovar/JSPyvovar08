96043098b03dcfdbb1311f6b3f37c428475e8646
dmytro26pyvovar <133111698+dmytro26pyvovar@users.noreply.github.com>
2023-09-26 20:56:40 +0200

    +

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,25 @@
+import Vimeo from './@vimeo/player';
+import throttle from './lodash.throttle';
+const iframe = document.querySelector('#vimeo-player');
+const player = new Vimeo(iframe, {
+});
+console.log(iframe)
+const onPlay = function (data) {
+    iframe.on('timeupdate',);
+    const currentTime = data.seconds;
+    localStorage.setItem('videoplayer-current-time', currentTime);
+};
 
+document.addEventListener('DOMContentLoaded', function () {
+    const savedTime = localStorage.getItem('videoplayer-current-time');
+    if (savedTime) {
+        player.setCurrentTime(parseFloat(savedTime));
+    }
+});
+const saveTime = throttle(function (time) {
+    localStorage.setItem('videoplayer-current-time', time);
+}, 1000);
+
+iframe.on('timeupdate', function (data) {
+    saveTime(currentTime);
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+import throttle from './lodash.throttle'
+function saveData() {
+    const userData = {
+        email: document.querySelector('input[name="email"]').value,
+        message: document.querySelector('textarea[name="message"]').value,
+    };
+    localStorage.setItem('feedback-form',JSON.stringify(formData));
+};
+
+function loadFormDataFromLocalStorage() {
+    const savedData = localStorage.getItem('feedback-form');
+    if (savedData) {
+        const formData = JSON.parse(savedData);
+        document.querySelector('input[name="email"]').value = userDataData.email;
+        document.querySelector('textarea[name="message"]').value = userDataData.message;
+    };
+
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const formData = {
+        email: document.querySelector('input[name="email"]').value,
+        message: document.querySelector('textarea[name="message"]').value,
+    };
+    console.log(formData);
+};
+
+localStorage.removeItem('feedback-form');
+
+document.querySelector('input[name="email"]').value = '';
+document.querySelector('textarea[name="message"]').value = '';
+
+};
+
+const form = document.querySelector('feedback-form');
+form.addEventListener('input', throttle(saveFormDataToLocalStorage, 500));
+form.addEventListener('submit', handleSubmit);
 
