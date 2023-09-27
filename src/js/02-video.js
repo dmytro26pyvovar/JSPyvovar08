680d4aa6be7a7aaee4a3745d910f0911e3907759
dmytro26pyvovar <133111698+dmytro26pyvovar@users.noreply.github.com>
2023-09-27 10:53:04 +0200

    +

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,4 +1,4 @@
-import Vimeo from '@vimeo/player';
+import Player from '@vimeo/player';
 import throttle from "lodash.throttle";
 const iframe = document.querySelector('#vimeo-player');
 const player = new Vimeo(iframe, {
