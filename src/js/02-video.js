e6b53e010cb6cc474927af86bdcf84d4c66ae5a4
dmytro26pyvovar <133111698+dmytro26pyvovar@users.noreply.github.com>
2023-09-27 11:30:40 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,24 +1,20 @@
 import Player from '@vimeo/player';
 import throttle from "lodash.throttle";
-const iframe = document.querySelector('#vimeo-player');
-const player = new Vimeo.Player(iframe);
-console.log(iframe)
-const onPlay = function (data) {
-    iframe.on('timeupdate',);
-    const currentTime = data.seconds;
-    localStorage.setItem('videoplayer-current-time', currentTime);
-};
 
 document.addEventListener('DOMContentLoaded', function () {
+    const iframe = document.querySelector('#vimeo-player');
+    const player = new Player(iframe);
+    let currentTime = 0;
     const savedTime = localStorage.getItem('videoplayer-current-time');
     if (savedTime) {
-        player.setCurrentTime(parseFloat(savedTime));
+        currentTime = parseFloat(savedTime);
     }
-});
-const saveTime = throttle(function (time) {
-    localStorage.setItem('videoplayer-current-time', time);
-}, 1000);
-
-iframe.on('timeupdate', function (data) {
-    saveTime(currentTime);
+      player.setCurrentTime(currentTime);
+    player.on('timeupdate', function (data) {
+        currentTime = data.seconds; 
+        saveTime(currentTime);
+    });
+    const saveTime = throttle(function (time) {
+        localStorage.setItem('videoplayer-current-time', time);
+    }, 1000);
 });
