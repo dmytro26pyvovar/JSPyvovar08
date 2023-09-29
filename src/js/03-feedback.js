0e0b67e146a349a51b0001996ec31f96d4f58e53
dmytro26pyvovar <133111698+dmytro26pyvovar@users.noreply.github.com>
2023-09-29 21:47:13 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,17 +4,17 @@ function saveData() {
         email: document.querySelector('input[name="email"]').value,
         message: document.querySelector('textarea[name="message"]').value,
     };
-    localStorage.setItem('feedback-form',JSON.stringify(formData));
+    localStorage.setItem('feedback-form',JSON.stringify(userData));
 };
 
 function loadFormDataFromLocalStorage() {
     const savedData = localStorage.getItem('feedback-form');
     if (savedData) {
         const formData = JSON.parse(savedData);
-        document.querySelector('input[name="email"]').value = userDataData.email;
-        document.querySelector('textarea[name="message"]').value = userDataData.message;
-    };
-
+        document.querySelector('input[name="email"]').value = formData.email; 
+        document.querySelector('textarea[name="message"]').value = formData.message;
+    }
+}
 
 function handleSubmit(event) {
     event.preventDefault();
@@ -22,17 +22,17 @@ function handleSubmit(event) {
         email: document.querySelector('input[name="email"]').value,
         message: document.querySelector('textarea[name="message"]').value,
     };
-    console.log(FormData);
+    console.log(formData);
+
+    localStorage.removeItem('.feedback-form');
 };
 
-localStorage.removeItem('feedback-form');
-
-document.querySelector('input[name="email"]').value = '';
-document.querySelector('textarea[name="message"]').value = '';
-
+function removeString(){
+    document.querySelector('input[name="email"]').value = '';
+    document.querySelector('textarea[name="message"]').value = '';
 };
 
 const form = document.querySelector('.feedback-form');
 form.addEventListener('input', throttle(saveData, 500));
 form.addEventListener('submit', handleSubmit);
-
+form.addEventListener('submit', removeString)
